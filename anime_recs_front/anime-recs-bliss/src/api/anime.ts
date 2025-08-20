97502369098b8f561a6c1ed21f0ef9aa8e5f2ea9
Z3ro-Kun <132244480+Z3ro-Kun@users.noreply.github.com>
2025-08-20 19:55:43 +0530

    Update anime.ts

--- anime_recs_front/anime-recs-bliss/src/api/anime.ts
+++ anime_recs_front/anime-recs-bliss/src/api/anime.ts
@@ -1,6 +1,6 @@
 // src/api/anime.ts
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Define the TypeScript type for the anime recommendation object
 // This matches the data structure from your Python backend
